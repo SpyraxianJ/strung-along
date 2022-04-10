abe930b49aefb6f302274769926903b41709c160
Ang Fu <fuang1016@gmail.com>
2022-04-11 01:27:00 +1000

    PauseMenu
    Basic pauseMenu added for weekly object

---
+++ Strung Along Full Unity Scene/Assets/PauseMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if (GameIsPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        //time back to normal;
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        //time freezed
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
