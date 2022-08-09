022b66f93036137e5eb870f62caa2ba77ee76dd9
Tim Beau David <66589884+TimBeauDavid@users.noreply.github.com>
2022-08-10 08:13:31 +1000

    Grid point generation started

---
+++ Strung Along Full Unity Scene/Assets/Scripts/Grid/GridGenerator.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+
+    public GridManager manager;
+    public GameObject pointPrefab;
+
+    [Space]
+
+    [Header("Grid Values")]
+
+    [Space]
+
+    [Tooltip("This is the dimentions of the grid in like, actual world size, not the resolution of it")]
+    public Vector2 gridSize;
+
+    [Space]
+
+    [Tooltip("The number of points along the X axis")]
+    [Range(1, 20)]
+    public int gridWidth;
+    [Tooltip("The number of points along the X axis")]
+    [Range(1, 20)]
+    public int gridDepth;
+
+    [Space]
+
+    [Tooltip("This is the position in world space the grid will be generated to")]
+    public Vector3 root;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid(true); // temp
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateGrid(bool destoryPoints)
+    {
+
+        if (destoryPoints) {
+            for (int i = 0; i < manager.points.Count; i++)
+            {
+                Destroy(manager.points[i].gameObject);
+            }
+        }
+
+        List<GridPoint> newPoints = new List<GridPoint>();
+
+        // This means we will write along the X axis each time
+        for (int z = 0; z < gridDepth; z++)
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                // The (x * 1f) and (z * 1f) are there so C# turns them into a float, there is like an actual command for it, but I forgot it, oops
+                Instantiate(pointPrefab, new Vector3(((x * 1f) / Mathf.Max(gridWidth - 1, 1) * gridSize.x) - gridSize.x/2, 0, ((z * 1f) / Mathf.Max(gridWidth - 1, 1) * gridSize.y) - gridSize.y / 2) + root, Quaternion.identity);
+            }
+        }
+
+    }
+}
