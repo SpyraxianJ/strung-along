916e602cfe2155c68d8c55179bf0a9072bec532e
harpyfox <harper.iro@gmail.com>
2022-08-18 20:43:10 +1000

    puppet ragdolls implemented
    not in level switching yet: thats next!

--- Strung Along Full Unity Scene/Assets/Scripts/Character/RagdollToggle.cs
+++ Strung Along Full Unity Scene/Assets/Scripts/Character/RagdollToggle.cs
@@ -9,6 +9,10 @@ public class RagdollToggle : MonoBehaviour
 	Collider[] _ragdollColliders; // list of colliders used for the ragdoll
 	Animator _anim;
 	
+	Vector3 _startPos;
+	
+	public Transform _hook;
+	
 	public bool ragdolled = false;
 	
     void Start() {
@@ -16,23 +20,38 @@ public class RagdollToggle : MonoBehaviour
 		
 		_anim = GetComponentInChildren<Animator>();
 		_ragdollColliders = _anim.GetComponentsInChildren<Collider>();
+		_hook.GetComponent<Rigidbody>().isKinematic = true;
+		
+		_startPos = _hook.localPosition;
 		
 		Off();
     }
 	
 	void Update() {
 		
-		if (ragdolled) On(); else Off();
+		// temporary for debug purpose, won't be in final implementation
+		if (ragdolled) On(false); else Off();
 		
 	}
 	
-	public void On() {
+	// activates ragdoll on this puppet.
+	// if "hooked" is true, the puppet is held at the hook transform position by their back.
+	// otherwise, goes totally floppy.
+	public void On(bool hooked) {
 		_gameplayCollider.enabled = false;
 		_anim.enabled = false;
 		
 		foreach (Collider c in _ragdollColliders) {
 			c.enabled = true;
 		}
+		
+		if (hooked) {
+			_hook.rotation = Quaternion.identity;
+		} else {
+			_hook.GetComponent<Rigidbody>().isKinematic = false;
+		}
+		
+		
 	}
 	
 	public void Off() {
@@ -42,6 +61,11 @@ public class RagdollToggle : MonoBehaviour
 		
 		_gameplayCollider.enabled = true;
 		_anim.enabled = true;
+		
+		_hook.GetComponent<Rigidbody>().isKinematic = true;
+		_hook.localPosition = _startPos;
 	}
+	
+	
 
 }
