189b3b1b58679e24967754311ab6983b1205a497
harpyfox <harper.iro@gmail.com>
2022-08-26 19:47:25 +1000

    better stage colliders, new light on intro cinematic

--- Strung Along Full Unity Scene/Assets/Scripts/Game/State Machine/LevelFirstPlayingState.cs
+++ Strung Along Full Unity Scene/Assets/Scripts/Game/State Machine/LevelFirstPlayingState.cs
@@ -13,6 +13,10 @@ public class LevelFirstPlayingState : LevelBaseState
 		
 		ctx._cinematics.PlayCutscene(ctx._cinematics._intro);
 		
+		// teleport under their string roots
+		ctx._player1.transform.position = ctx._player1.GetComponent<PuppetController>().thisStringRoot.transform.position + new Vector3(0, -4, 0);
+		ctx._player2.transform.position = ctx._player2.GetComponent<PuppetController>().thisStringRoot.transform.position + new Vector3(0, -4, 0);
+
 	}
 
 	public override void UpdateState(GameStateManager ctx) {
@@ -22,6 +26,9 @@ public class LevelFirstPlayingState : LevelBaseState
 			// disable movement.
 			ctx._player1.GetComponent<PuppetController>().move = Vector2.zero;
 			ctx._player2.GetComponent<PuppetController>().move = Vector2.zero;
+			// they can still jump though because its funny
+			ctx._player1.GetComponent<Rigidbody>().velocity.Scale( new Vector3(0f, 1f, 0f) );
+			ctx._player2.GetComponent<Rigidbody>().velocity.Scale( new Vector3(0f, 1f, 0f) );
 			
 			// make players look forwards.
 			Vector3 lookPosition = new Vector3( ctx._camera.Position().x, 0f, ctx._camera.Position().z );
