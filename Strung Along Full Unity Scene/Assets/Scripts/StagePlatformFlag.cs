d0e20a0d1e1b6117c11e90ca4c78582b122b7cd6
Dylan Jarvis <jarvisdj@optusnet.com.au>
2022-09-01 21:08:13 +1000

    Added water control stage tile script

---
+++ Strung Along Full Unity Scene/Assets/Scripts/StagePlatformFlag.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagePlatformFlag : MonoBehaviour
+{
+    //this is a flag so the object with this attached can be easily found using the GetObjectsOfType() function
+}
